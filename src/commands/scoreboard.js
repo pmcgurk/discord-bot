ce9f80e711badb92cc98b8a4a9b4ab57bf9d8457
Paul McGurk <paul@preka.digital>
2021-12-12 12:20:52 +0000

    removed unused params

--- src/commands/scoreboard.js
+++ src/commands/scoreboard.js
@@ -1,9 +1,9 @@
 import getScoreboard from '../db/getScoreboard';
 import formatAmount from '../lib/formatAmount';
+import getUsername from '../lib/getUsername';
 
-const scoreboard = async (msg) => {
-  const sender = msg.author;
-  const currentScoreboard = await getScoreboard(sender.id, msg);
+const scoreboard = async () => {
+  const currentScoreboard = await getScoreboard();
   if (currentScoreboard) {
     const formattedScoreboard = currentScoreboard.map((user, index) => (`${index + 1}: ${user.name}: ${formatAmount(user.amount)}\n`));
     return `Here's the scoreboard as of now:
