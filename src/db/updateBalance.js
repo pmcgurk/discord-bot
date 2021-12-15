06752c3d689825dbed9075a1601aa2585281e219
Paul McGurk <paul@preka.digital>
2021-12-15 12:01:09 +0000

    made 0 the limit

--- src/db/updateBalance.js
+++ src/db/updateBalance.js
@@ -1,10 +1,14 @@
 import Transaction from '../models/transaction';
 import User from '../models/user';
 
-export default async function sendTransaction(UserId, amount, note = false) {
-  const user = await User.query().where({ id: UserId }).increment('amount', amount).returning('*');
-  if (note) {
-    await Transaction.query().insert({ SenderId: null, ReceiverId: UserId, amount, note });
+export default async function updateBalance(UserId, amount, note = false) {
+  const curUser = await User.query().findById(UserId);
+  if (curUser.amount + amount >= 0 || amount > 0) {
+    const user = await User.query().where({ id: UserId }).increment('amount', amount).returning('*');
+    if (note) {
+      await Transaction.query().insert({ SenderId: null, ReceiverId: UserId, amount, note });
+    }
+    return user[0];
   }
-  return user[0];
+  return curUser[0];
 }
