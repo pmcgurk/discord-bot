51220367c9482d8d6493f210f1d9a61e1ed87c00
Paul McGurk <paul@preka.digital>
2021-12-16 17:36:13 +0000

    fixed crash

--- src/db/getScoreboard.js
+++ src/db/getScoreboard.js
@@ -1,6 +1,6 @@
 import User from '../models/user';
 
 export default async function getScoreboard() {
-  const users = await User.query().orderBy('amount', 'DESC').whereNot({ id: '918900485484851251' }).whereNot('deletedAt');
+  const users = await User.query().orderBy('amount', 'DESC').whereNot({ id: '918900485484851251' }).whereNull('deletedAt');
   return users;
 }
