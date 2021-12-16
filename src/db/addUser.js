ab4d225bc44832c927c679bdb19372526de9644b
Paul McGurk <paul@preka.digital>
2021-12-16 17:44:56 +0000

    fix for user crash

--- src/db/addUser.js
+++ src/db/addUser.js
@@ -2,6 +2,6 @@ import getUsername from '../lib/getUsername';
 import User from '../models/user';
 
 export default async function addUser(UserId, startAmount = 0) {
-  const newUser = { id: UserId, name: await getUsername(UserId), amount: startAmount };
+  const newUser = { id: UserId, name: 'New User', amount: startAmount };
   return User.query().insert(newUser);
 }
