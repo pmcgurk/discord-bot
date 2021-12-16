04d790c3e0bc91ddeeecc326bdda025770731b34
Paul McGurk <paul@preka.digital>
2021-12-16 17:30:54 +0000

    fixed bug

--- src/db/addUser.js
+++ src/db/addUser.js
@@ -1,7 +1,7 @@
 import getUsername from '../lib/getUsername';
 import User from '../models/user';
 
-export default async function addUser(UserId, startAmount = 1000) {
-  const newUser = { id: UserId, name: 'New User', amount: startAmount };
+export default async function addUser(UserId, startAmount = 0) {
+  const newUser = { id: UserId, name: await getUsername(UserId), amount: startAmount };
   return User.query().insert(newUser);
 }
